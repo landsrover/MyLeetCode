084f3a85c812e4cfa059d4c6d082f72b6c2de258
songjing <jingsong4work@gmail.com>
2016-06-24 21:32:42 +1000

    break to get result at the first time

--- two-sum-new.cpp
+++ two-sum-new.cpp
@@ -3,20 +3,21 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
         unordered_map<int,int> rev;
+        int gap;
+        vector<int> ret;
+        
         for (auto i=0; i<nums.size(); i++)
         {
             rev[nums[i]] = i;
         }
-        
-        int gap;
-        vector<int> ret;
-        
+
         for (auto i=0; i<rev.size(); i++)
         {
             gap = target - nums[i];
             if (rev.find(gap) != rev.end() && rev[gap] > i)
             {
-                ret = {i,rev[gap]};        
+                ret = {i,rev[gap]};  
+                break;
             }
         }
         return ret;
